28da73f3bf4326a82fc891191f4f9e6f7a48d776
lagonza8 <official.lag.90@gmail.com>
2021-11-17 07:23:11 -0700

    Adds diagonal length message to LetterSizeConversion class in Chapter 2 package.

--- src/com/example/chaptertwo/LetterSizeConversion.java
+++ src/com/example/chaptertwo/LetterSizeConversion.java
@@ -1,7 +1,8 @@
 package com.example.chaptertwo;
-import javax.swing.JOptionPane;
+
 
 import static javax.swing.JOptionPane.*;
+
 
 public class LetterSizeConversion {
     public static void main(String[] args) {
@@ -12,10 +13,25 @@ public class LetterSizeConversion {
         Double widthPaperInches = 8.5;
         Double lengthPaperInches = 11.0;
 
-        showMessageDialog(null, "Dimensions in mm: " + widthPaperInches*mmPerInch + "mm by " + lengthPaperInches*mmPerInch + " mm.");
+        showMessageDialog(null,
+                "Dimensions in mm: " + widthPaperInches*mmPerInch + "mm by " + lengthPaperInches*mmPerInch + " mm.");
 
-        showMessageDialog(null, "Perimeter in mm: " + ((2*widthPaperInches*lengthPaperInches) + (2*lengthPaperInches*mmPerInch)));
+        showMessageDialog(null,
+                "Perimeter in mm: " + ((2*widthPaperInches*lengthPaperInches) + (2*lengthPaperInches*mmPerInch)));
 
+        // Find the diagonal length
+        // diagonal length is basically Pythagorean theorem
+        Double mmWidth = widthPaperInches * mmPerInch;
+        Double mmLength = lengthPaperInches * mmPerInch;
 
+        Double widthSquared = Math.pow(mmWidth, 2.0);
+        Double lengthSquared = Math.pow(mmLength, 2.0);
+
+        Double additionOfSquares = widthSquared + lengthSquared;
+
+        Double diagonal = Math.sqrt(additionOfSquares);
+
+        showMessageDialog(null,
+                "Diagonal length: " + diagonal);
     }
 }
