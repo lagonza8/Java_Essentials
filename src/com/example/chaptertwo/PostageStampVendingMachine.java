361d24dbe9fa4c91df0e717b1a1be0004fb3458c
lagonza8 <official.lag.90@gmail.com>
2021-11-16 19:59:21 -0700

    Adds multiple Chapter 2 class examples.

--- src/com/example/chaptertwo/PostageStampVendingMachine.java
+++ src/com/example/chaptertwo/PostageStampVendingMachine.java
@@ -1,4 +1,27 @@
 package com.example.chaptertwo;
+import javax.swing.JOptionPane;
 
 public class PostageStampVendingMachine {
+    public static void main(String[] args) {
+        // Ask the user for current price of 1st class stamps in pennies, parse the answer
+        String firstClass = JOptionPane.showInputDialog("Enter the price of 1st class postage stamps (in pennies): ");
+        Integer pricePerStamp = Integer.parseInt(firstClass);
+        JOptionPane.showMessageDialog(null, "Price: " + pricePerStamp);
+
+        // we can use integer division and modulus to get the number of stamps purchased and change in penny stamps
+        Integer dollars, firstClassStamps, change;
+
+        String unitProvided = JOptionPane.showInputDialog("Enter payment denomination (bill/note value): ");
+        dollars = Integer.parseInt(unitProvided);
+        Integer payment = dollars * 100;
+
+        firstClassStamps = payment / pricePerStamp;
+        change = (payment - (firstClassStamps * pricePerStamp));
+
+        System.out.printf("First class stamps: %6d%n", firstClassStamps);
+        System.out.printf("Penny stamps:       %6d%n", change);
+
+        JOptionPane.showMessageDialog(null, "First class stamps: " + firstClassStamps);
+        JOptionPane.showMessageDialog(null, "Penny stamps: " + change);
+    }
 }
