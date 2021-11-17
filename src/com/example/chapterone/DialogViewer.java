aca5d16abaeb4c1c1a6ad17be758f841ce53293b
lagonza8 <official.lag.90@gmail.com>
2021-11-16 19:01:36 -0700

    Adds multiple Chapter 2 class examples.

--- src/com/example/chapterone/DialogViewer.java
+++ src/com/example/chapterone/DialogViewer.java
@@ -4,8 +4,19 @@ import javax.swing.JOptionPane;
 public class DialogViewer {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hello, LUis!");
+        // the second argument can be an expression as long as we have one string in the concatenation expr.
+
+        // show the user the input dialog requirement pane, save their answer, do something useful with it
         String name = JOptionPane.showInputDialog("What is your name?");
         System.out.println("Hello, " + name + "!");
+
+        // parse the value provided after its requested appropriately
+        String priceInput = JOptionPane.showInputDialog("Enter your price: " );
+        Double price = Double.parseDouble(priceInput); // static method call, no object/instance needed
+        // display the output in a dialog box
+        JOptionPane.showMessageDialog(null, "Price entered: " + price);
+
+        JOptionPane.showMessageDialog(null, "Hey there: ");
 
     }
 }
