9b18105c14b8e175581a9b1d80afc5df742d55b4
lagonza8 <official.lag.90@gmail.com>
2021-11-17 08:23:58 -0700

    Adds TwoIntegerComparison class to Chapter 2 package.

---
+++ src/com/example/chaptertwo/TwoIntegerComparison.java
@@ -0,0 +1,31 @@
+package com.example.chaptertwo;
+import javax.swing.JOptionPane;
+
+public class TwoIntegerComparison {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Please enter two integer values for comparison.");
+        String firstValue = JOptionPane.showInputDialog("Enter the first integer: ");
+        Integer value1 = Integer.parseInt(firstValue);
+        String secondValue = JOptionPane.showInputDialog("Enter the second integer: ");
+        Integer value2 = Integer.parseInt(secondValue);
+
+        JOptionPane.showMessageDialog(null,
+                "Their sum is: " + (value1 + value2));
+        JOptionPane.showMessageDialog(null,
+                "Their difference is: " + (value1 - value2));
+        JOptionPane.showMessageDialog(null,
+                "Their product is: " + (value1 * value2));
+        JOptionPane.showMessageDialog(null,
+                "Their average is: " + ((value1 + value2)/2.0));
+        JOptionPane.showMessageDialog(null,
+                "Their distance is: " + (Math.abs((value1 - value2))));
+
+        JOptionPane.showMessageDialog(null,
+                "The larger value is: " + (Math.max(value1, value2)));
+        JOptionPane.showMessageDialog(null,
+                "The smaller value is: " + (Math.min(value1, value2)));
+
+
+    }
+}
