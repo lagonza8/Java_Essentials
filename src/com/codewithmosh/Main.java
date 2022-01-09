6bd7f09aa753864c2e869689e7478524550e85c5
lagonza8 <official.lag.90@gmail.com>
2022-01-09 10:46:38 -0700

    Adds complete course code for the YouTube video.

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,17 +1,20 @@
 package com.codewithmosh;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
         // byte data type is used when integer's may take up more memory when its not needed
-        int age = 30;               // int range: about [-2B, 2B]
-        short shortAge = 30;        // 2 bytes, range [-32K, 32K]
         byte byteAge = 30;          // byte values range from -128 - +127, appropriate for most age values
+        short shortAge = 30;        // 2 bytes, range [-32K, 32K]
+        int age = 30;               // int range: about [-2B, 2B]
 
         // you can use underscore in place of a comma for larger values
         long viewsCount = 123456789;
@@ -20,22 +23,74 @@ public class Main {
          java interprets both values above as integer data types even though they were declared as "long" data type
          it's because they are still within the valid range of being a value represented by int data type
         */
-        long viewsCount3 = 3_123_456_789L; // valid, used to exceed int value ranges
+        long viewsCount3 = 3_123_456_789L; // valid, used to exceed int value ranges, modifier IS REQUIRED
 
         float price = 10.99F;
-        // interpreted as a double without the format specifier and would give compilation error
+        // interpreted as a double without the format modifier and would give compilation error otherwise
 
         char letter = 'A';
 
         boolean isEligible = false;
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        /*while loops are good when we don't know ahead of time how many process repitions are required*/
+        Scanner repeatScanner = new Scanner(System.in);
+        String input = "";
+        while(true/*!input.equals("quit")*/){
+            System.out.print("Input: ");
+            input = repeatScanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue; /*move control to beginning of loop*/
+            if (input.equals("quit"))
+                break; /*jumps you out of the loop*/
+            /*every infinite loop must have a break statement/termination statement*/
+            System.out.println(input);
+        }
+
+        // This will be executed at least once, then condition is checked
+        do{
+            System.out.print("Input(do-while): ");
+            input = repeatScanner.next().toLowerCase();
+            System.out.println(input);
+        } while(!input.equals("quit"));
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        String[] fruits = {"Apples", "Mango", "Oranges"};
+
+        for (int i = fruits.length - 1; i >= 0; i--) {
+            System.out.println(fruits[i]);
+        }
+
+        /*Enhanced for loops (For-Each Loops) only iterate in a forward direction*/
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
         Date now = new Date();
-        System.out.print("Passing a Date object's .getTime() method to the print statement: ");
+        System.out.print("Passing a Date object's .getTime() method result (return value) to the print statement: ");
         System.out.println(now.getTime());
+        System.out.println("^^ It certainly has a strange output.");
 
         System.out.print("Passing a Date object to the print statement: ");
-        System.out.println(now);
+        System.out.println(now + "... ahhh, much better.");
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
         byte x = 1;
         byte y = x; // both variables store the value 1 at this point, each in separate memory locations
@@ -58,13 +113,19 @@ public class Main {
          primitive types are referenced by their value
         */
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
         String message = new String("Hello world." + "!@!"); // valid, but redundant
         String shortMessage = "Hello world.";
         String untrimmedMessage = "    Hello world   !!   ";
 
-        System.out.println(message.endsWith("!"));
+        System.out.println("Does \"Hello world.!@!\" end with \"!\" ?: " + message.endsWith("!"));
+        System.out.print("The number of characters in \"Hello world.\" is: ");
         System.out.println(shortMessage.length());
+        System.out.print("The letter 'H' in \"Hello world.\" is found at character array index value: ");
         System.out.println(message.indexOf("H"));
 
         // Strings are immutable objects in Java, when you modify a string, a new string is created and returned
@@ -81,6 +142,21 @@ public class Main {
          This is an example of the escape sequence.
          tab, new line, double/single quote, double backslash to represent backslash in string
         */
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        for(int i = 1; i <= 5; i++)
+            System.out.println("Hello World " + i);
+
+        for(int i = 5; i > 0; i--)
+            System.out.println("Hello World " + i);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
 
         int[] numbers = new int[5];
         numbers[0] = 1;
@@ -128,6 +204,7 @@ public class Main {
         System.out.println(twoByTwox2[0][0].length);
 
 
+
         int moduloInteger = 1776;
         System.out.print("1776 % 10 returns the last digit in the value... ");
         System.out.println(moduloInteger % 10);
@@ -145,6 +222,86 @@ public class Main {
         xyz /= 10; // xyz = 9
 
         System.out.println(xyz);
+
+        NumberFormat percent = NumberFormat.getPercentInstance(); // abstract class instantiated with a method/member
+        String result = percent.format(0.1);
+        System.out.println(result);
+
+        // or we can method chain to condense the code above
+        String secondResult = NumberFormat.getPercentInstance().format(0.2);
+        System.out.println(secondResult);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.print("Age: ");
+        byte ageByte = ageScanner.nextByte();
+        System.out.println("You are " + ageByte);
+
+        /*trim will delete white space before and after the main String*/
+        Scanner nameScanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = nameScanner.nextLine().trim();
+        System.out.println("You are " + name);
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        /*This is the mortgage calculator*/
+        final byte MONTHS_IN_A_YEAR = 12;
+        final byte PERCENT = 100;
+        /*Calculate mortgage payments*/
+        double principleAmount_P = 100000.00;
+        double rateAnnual_R = 3.92;
+        byte years_N = 30;
+        /*Not sure of the consequences of initializing the variable above*/
+//      Second level
+        double convertedRate_r = rateAnnual_R / PERCENT/ MONTHS_IN_A_YEAR;
+        double oPR_1_plus_r = (1 + convertedRate_r);
+        int numberOfPayments_n = years_N * MONTHS_IN_A_YEAR;
+//      Got rid of many magic numbers
+        double mortgage = principleAmount_P * ((convertedRate_r*Math.pow(oPR_1_plus_r, numberOfPayments_n))/(Math.pow(oPR_1_plus_r, numberOfPayments_n)-1));
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("You will pay " + mortgageFormatted + " for the mortgage loan.");
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        Scanner incomeScanner = new Scanner(System.in);
+        System.out.println("Enter your income rounded to nearest dollar amount: ");
+        int income = incomeScanner.nextInt();
+        String className = (income > 100_000) ? "First" : "Economy";
+        System.out.println("You are in " + className + " class. Thank you for flying Mohegan Airlines.");
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+        /*A popular interview question*/
+        Scanner valueScanner = new Scanner(System.in);
+        System.out.print("Enter an integer value :");
+        int valueEntered = valueScanner.nextInt();
+
+        if(((valueEntered % 5) == 0) && ((valueEntered % 3) == 0))
+            System.out.println("FizzBuzz");
+        else if((valueEntered % 5) == 0)
+            System.out.println("Fizz");
+        else if((valueEntered % 3) == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(valueEntered);
+
+        /*Don't Repeat Yourself (D.R.Y.), only when you have to.*/
+
 
     }
 }
